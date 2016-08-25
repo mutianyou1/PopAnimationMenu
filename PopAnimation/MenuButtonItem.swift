a64e969b66cc39385bda6e2ab91f983d9310b40c
(外包) 潘元荣 <ex-panyuanrong001@c02r52txfvh5.local>
2016-08-25 09:16:05 +0800

    color and title height

--- PopAnimation/MenuButtonItem.swift
+++ PopAnimation/MenuButtonItem.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 private let KItemButtonHeight :CGFloat = 80.0
+private let KHeightScale : CGFloat = UIScreen.mainScreen().bounds.height/667.0
 class MenuButtonItem: UIControl {
     
     private let button = UIButton.init(type: UIButtonType.Custom)
@@ -36,7 +37,8 @@ class MenuButtonItem: UIControl {
         button.frame.size = CGSizeMake(KItemButtonHeight, KItemButtonHeight)
         button.center = CGPointMake(self.bounds.size.width*0.5, self.bounds.size.height*0.5)
         button.addTarget(self, action: "clickOnButton", forControlEvents: UIControlEvents.TouchUpInside)
-        title.frame = CGRectMake(0, self.bounds.size.height - 20, self.bounds.size.width, 20)
+        title.frame = CGRectMake(0, self.bounds.size.height - 20 * KHeightScale, self.bounds.size.width, 20*KHeightScale)
+        title.font = UIFont.systemFontOfSize(18.0*KHeightScale)
         title.textColor = UIColor.lightGrayColor()
         title.textAlignment = NSTextAlignment.Center
         self.addSubview(button)
@@ -54,7 +56,7 @@ class MenuButtonItem: UIControl {
     func getButtonImageColor() -> UIColor{
         let pixeData = CGDataProviderCopyData(CGImageGetDataProvider(self.image.CGImage))
         let data :UnsafePointer<UInt8> = CFDataGetBytePtr(pixeData)
-        let pixeInfo : Int = ((Int(self.bounds.size.width) * Int(20)) + Int(20)) * 4
+        let pixeInfo : Int = ((Int(self.bounds.size.width) * Int(100)) + Int(100)) * 4
         return UIColor.init(red: CGFloat.init(data[pixeInfo])/255.0, green: CGFloat.init(data[pixeInfo+1])/255.0, blue: CGFloat.init(data[pixeInfo+2])/255.0, alpha: CGFloat.init(data[pixeInfo+3])/255.0)
     }
     required init?(coder aDecoder: NSCoder) {
